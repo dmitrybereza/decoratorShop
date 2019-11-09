c3f45349fe076fef9006970bc4eaae4cf89c811e
Bereza Dmitry <dmitrybereza2001@gamil.com>
2019-11-09 22:18:36 +0200

    maybe all

--- Accets/Js/cardScript.js
+++ Accets/Js/cardScript.js
@@ -235,3 +235,11 @@ function deleteFromCard(){
 	saveCard();
 	showCard();
 }
+
+$(".txtb input").on("focus",function(){
+	$(this).addClass("focus");
+})
+$(".txtb input").on("blur",function(){
+	if($(this).val() == "")
+	$(this).removeClass("focus");
+})
