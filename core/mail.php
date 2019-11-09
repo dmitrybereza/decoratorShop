fa851cb95be7c44e6fc699c6b2aaf2ebe830b68b
Bereza Dmitry <dmitrybereza2001@gamil.com>
2019-11-09 19:52:08 +0200

    mail is done

--- core/mail.php
+++ core/mail.php
@@ -1,21 +1,33 @@
 <?php
 
 $json = file_get_contents('../goods.json');
-$json = json_decode($json);
+$json = json_decode($json, true);
 
 $message = '';
 $message .= '<h1>Заказ в магазине</h1>';
 $message .= '<p>ФИО: '.$_POST['eFIO'].'</p>';
-$message .= '<p>Номер телефона: '.$_POST['eNum'].'</p>';
+$message .= '<p>Номер телефона: '.$_POST['eNUM'].'</p>';
 $message .= '<p>Электронная почта: '.$_POST['email'].'</p>';
 $message .= '<p>Город: '.$_POST['eCity'].'</p>';
 $message .= '<p>Отделение Новой почты: '.$_POST['ePost'].'</p>';
 
-$goods = $_POST['card'];
-
+$card = $_POST['card'];
+$sum = 0;
 foreach($card as $id=>$count){
-    $message .=$json[$id][$name];
+    $message .=$json[$id]['name'].' ----- ';
+    $message .=$count.' ----- ';
+    $message .=$count*$json[$id]['cost'];
+    $message .='<br>';
+    $sum = $sum + $count*$json[$id]['cost'];
 }
+$message .='Всего: '.$sum.' Грн';
 
-print_r($message);
+$to = 'dmitrybereza2001@gmail.com'.',';
+$to .= $_POST['email'];
+$spectext ='<!DOCKTYPE HTML><html><head><title>Заказ</title></head><body>';
+$headers ='MIME-Version: 1.0'."\r\n";
+$headers .= 'Content-type: text/html; charset=utf-8'."\r\n";
+
+$m = mail($to, 'Заказ в магазине', $spectext.$message.'</body></html>', $headers);
+if($m) {echo 1;} else {echo 0;}
 ?>
