5222dda7089c83b2b14b67fc911f24cbefea7065
Bereza Dmitry <dmitrybereza2001@gamil.com>
2019-11-09 15:24:15 +0200

    first commit

---
+++ Accets/Js/cardScript.js
@@ -0,0 +1,180 @@
+var card ={
+};
+
+$("document").ready(function($){
+	var nav = $('.header');
+  var nav2 = $('.contentContainer');
+  var nav3 = $('.upperMenu');
+  var nav4 = $('.badge');
+	var nav5 = $('.logo');
+	console.log("hi");
+  var btn = $('#goTopButton');
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 15) {
+			nav.addClass("headerNew")
+      nav2.addClass("contentContainerNew")
+      nav3.addClass("upperMenuNew")
+      nav4.addClass("badgeNew")
+			nav5.addClass("logoNew")
+		} else {
+			nav.removeClass("headerNew")
+      nav.removeClass("contentContainerNew")
+      nav3.removeClass("upperMenuNew")
+      nav4.removeClass("badgeNew")
+			nav5.removeClass("logoNew")
+		}
+    if($(this).scrollTop() > 100){
+          $('#goTopButton').fadeIn();
+    } else {
+        $('#goTopButton').fadeOut();
+    }
+	});
+  btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, 800)
+  });
+
+});
+
+function onload(){
+	loadCard();
+	loadCount();
+}
+
+function loadCard(){
+	if(localStorage.getItem('card')){
+		card = JSON.parse(localStorage.getItem('card'));
+		showCard();
+	}
+	else{
+		$('.cardList').html('Корзина пуста!')
+	}
+}
+
+
+function showCard()
+{
+	$.getJSON('goods.json',function(data){
+		var out = '';
+		if(Object.keys(card).length == 0){
+			out+='<div class="emptyCard">';
+			out+='<p>Корзина пустая</p>';
+			out+='</div>';
+			$('.cardList').html(out);
+		}
+		else{
+		var goods = data;
+		var sum = 0;
+		for(var id in card){
+			out+=`<div class ="cardItem">`;
+			out+='<div class="itemImgCard">';
+			out+=`<img src="Accets/Img/${goods[id].img}">`;
+			out+='</div>';
+			out+='<div class ="ItemNameCard">';
+			out+='</div>';
+            out+='<div class ="ItemAmountInfo">';
+            out+='<p>'+goods[id].name+'</p>';
+            out+='</div>'
+            out+='<div class ="ItemButtons">';
+			out+=`<button onclick="incrShopCard()" data-id ="${id}" class="add-to-card"><i class="fas fa-plus"></i></button>`;
+			out+=`<p>${card[id]}</p>`;
+			out+=`<button onclick="decrShopCard()" data-id ="${id}" class ="delete-one-from-card"><i class="fas fa-minus"></i></button>`;
+			out+='<p>'+card[id]*goods[id].cost+'</p>';
+			sum+=card[id]*goods[id].cost;
+			out+=`<button data-id ="${id}" class ="delete-from-card"><i class="fas fa-trash"></i></button>`;
+			out+='</div>';
+			out+='</div>';
+		}
+		out+='<div class="sum">';
+		out+='<p>Итого: '
+		out+='<div class ="sumNum">'
+		out+='<p>'+ sum +' ГРН</p>';
+		out+='</div>';
+		out+='</div>';
+        out+='<div>';
+        out+='<button onclick="confirmOrder()" class="ConfirmOrder">Оформить заказ</button>';
+        out+='</div>';
+		$('.cardList').html(out);
+		$('.add-to-card').on('click', addToCardFromCard);
+		$('.delete-one-from-card').on('click', deleteOneFromCard);
+		$('.delete-from-card').on('click', deleteFromCard);
+	}
+	});
+}
+
+function confirmOrder(){
+	var out = '';
+	var order = jQuery('.ConfirmOrder')
+	order.detach();
+	out+='<div class ="confirmOrderForm">';
+	out+='<div class ="formFields">';
+	out+='<input placeholder="e-mail"></input>'
+	out+='<input placeholder="ФИО"></input>'
+	out+='<input placeholder="Номер телефона"></input>'
+	out+='<input placeholder="Город"></input>'
+	out+='<input placeholder="Отделение доставки Новой Почты"></input>'
+	out+='</div>';
+	out+='</div>';
+	$(out).appendTo('.cardList');
+}
+
+function addToCardFromCard(){
+	var id =$(this).attr('data-id');
+	card[id]++;
+	saveCard();
+	showCard();
+}
+
+function saveCard(){
+	localStorage.setItem('card',JSON.stringify(card));
+}
+
+function loadCount(){
+	if(localStorage.getItem('counter')){
+		count = localStorage.getItem('counter');
+		document.getElementById("lblCartCount").textContent = count;
+	}
+	else {
+		document.getElementById("lblCartCount").textContent = 0;
+	}
+}
+var count = 0;
+function incrShopCard(){
+	count++;
+	localStorage.setItem('counter',count);
+	document.getElementById("lblCartCount").textContent = count;
+}
+
+function decrShopCard(){
+	if(count ==0){}
+	else{
+		count--;
+		localStorage.setItem('counter',count);
+		document.getElementById("lblCartCount").textContent = count;
+	}
+}
+
+
+function deleteOneFromCard(){
+	var id =$(this).attr('data-id');
+		card[id]--;
+		if(card[id]==0)
+		{
+			delete card[id];
+		}
+	saveCard();	
+	showCard();
+}
+
+function deleteFromCard(){
+	var id =$(this).attr('data-id');
+	do {
+		card[id]--;
+        count--;
+        localStorage.setItem('counter',count);
+		document.getElementById("lblCartCount").textContent = count;
+    } while (card[id] != 0);
+    delete card[id];
+	saveCard();
+	showCard();
+}
---
+++ Accets/Js/mainPageScript.js
@@ -0,0 +1,119 @@
+var card ={
+};
+
+function onload(){
+	init();
+	loadCard();
+	loadCount();
+}
+
+function openInNewTab(url){
+	var win = window.open(url,'_self');
+	win.focus();
+}
+
+$("document").ready(function($){
+	var nav = $('.header');
+  var nav2 = $('.contentContainer');
+  var nav3 = $('.upperMenu');
+  var nav4 = $('.badge');
+	var nav5 = $('.logo');
+	console.log("hi");
+  var btn = $('#goTopButton');
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 15) {
+			nav.addClass("headerNew")
+      nav2.addClass("contentContainerNew")
+      nav3.addClass("upperMenuNew")
+      nav4.addClass("badgeNew")
+			nav5.addClass("logoNew")
+		} else {
+			nav.removeClass("headerNew")
+      nav.removeClass("contentContainerNew")
+      nav3.removeClass("upperMenuNew")
+      nav4.removeClass("badgeNew")
+			nav5.removeClass("logoNew")
+		}
+    if($(this).scrollTop() > 100){
+          $('#goTopButton').fadeIn();
+    } else {
+        $('#goTopButton').fadeOut();
+    }
+	});
+  btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, 800)
+  });
+
+});
+
+
+function init() {
+$.getJSON("goods.json", shopMenu);
+}
+
+function shopMenu(data){
+  console.log("hi");
+  var out='';
+  for (var key in data) {
+		out+=' <div class="itemShop">';
+		out+='<div class="itemName"><p>' + data[key].name + '</p></div>';
+		out+='<div class="itemImj">';
+		out+='<img src="Accets/Img/' + data[key].img + '" alt="" class="shopImg">';
+		out+='</div>';
+		out+='<div class="itemDesc">';
+		out+='<p>'+ data[key].description +'</p><div class="objom">';
+		out+='<p>'+data[key].value +'</p></div>';
+		out+='</div>';
+		out+='<div class="itemInfo">';
+		out+='<div class="itemPrice">'+data[key].cost +' Грн</div>';
+		out+=`<button onclick="incrShopCard()" data-id ="${key}" class="add-to-card"type="button" name="button">В корзину</button>`;
+		out+='</div>';
+		out+='</div>';
+  }
+  document.getElementsByClassName("shopMenu")[0].innerHTML=out;
+	$('.add-to-card').on('click', addToCard);
+
+}
+
+function addToCard(){
+	var id =$(this).attr('data-id');
+	if(card[id] == undefined){
+		card[id] = 1;
+	}
+	else {
+		card[id]++;
+	}
+	saveCard();
+}
+
+
+function loadCard(){
+	if(localStorage.getItem('card')){
+		card = JSON.parse(localStorage.getItem('card'));
+	}
+	else{
+		$('.cardList').html('Корзина пуста!')
+	}
+}
+
+function saveCard(){
+	localStorage.setItem('card',JSON.stringify(card));
+}
+
+function loadCount(){
+	if(localStorage.getItem('counter')){
+		count = localStorage.getItem('counter');
+		document.getElementById("lblCartCount").textContent = count;
+	}
+	else {
+		document.getElementById("lblCartCount").textContent = 0;
+	}
+}
+var count = 0;
+function incrShopCard(){
+	count++;
+	localStorage.setItem('counter',count);
+	document.getElementById("lblCartCount").textContent = count;
+}
+
